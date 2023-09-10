251ce22ea42404e86e4c306ee4970a6b029e1690
dasu <kirill12093@mail.ru>
2023-09-11 02:11:48 +0300

    lab1: done

---
+++ lab1/db_scheme.sql
@@ -0,0 +1,31 @@
+create table services
+(
+    id           serial primary key,
+    name         varchar(50),
+    cost_our     numeric(18, 2),
+    cost_foreign numeric(18, 2)
+);
+
+create table cars
+(
+    id         serial primary key,
+    num        varchar(20),
+    color      varchar(20),
+    mark       varchar(20),
+    is_foreign varchar(1)
+);
+
+create table masters
+(
+    id   serial primary key,
+    name varchar(50)
+);
+
+create table works
+(
+    id         serial primary key,
+    date_work  date,
+    master_id  integer references masters (id),
+    car_id     integer references cars (id),
+    service_id integer references services (id)
+);
