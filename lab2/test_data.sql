aa3759d7b308fa3b44d5f9e2a9ecaeb9c19a7f06
dasu <kirill12093@mail.ru>
2023-09-11 20:35:23 +0300

    lab2: add test data for tables

---
+++ lab2/test_data.sql
@@ -0,0 +1,56 @@
+insert into masters(name)
+values ('Oleg'),
+       ('Pavel'),
+       ('Erdem'),
+       ('Alexander'),
+       ('Zhora'),
+       ('Ashot'),
+       ('Ibragim'),
+       ('Islam');
+
+INSERT INTO cars (num, color, mark, is_foreign)
+VALUES ('061', 'Red', 'Toyota', false),
+       ('702', 'Blue', 'Honda', false),
+       ('123', 'Green', 'Ford', false),
+       ('344', 'Silver', 'Chevrolet', false),
+       ('565', 'Black', 'Nissan', false),
+       ('986', 'White', 'Volkswagen', true),
+       ('127', 'Gray', 'BMW', true),
+       ('098', 'Yellow', 'Mercedes-Benz', true),
+       ('922', 'Orange', 'Audi', true),
+       ('666', 'Purple', 'Lamborghini', true),
+       ('777', 'Brown', 'Porsche', true),
+       ('112', 'Pink', 'Ferrari', true),
+       ('228', 'Cyan', 'Maserati', true),
+       ('148', 'Magenta', 'Tesla', true),
+       ('678', 'Lime', 'Kia', false),
+       ('223', 'Turquoise', 'Subaru', false),
+       ('417', 'Beige', 'Hyundai', false),
+       ('621', 'Gold', 'Lexus', true),
+       ('719', 'Silver', 'Jeep', true),
+       ('520', 'Bronze', 'Cadillac', true);
+
+INSERT INTO services (name, cost_our, cost_foreign)
+VALUES
+    ('Kirishi Auto Service', 1500, 2200),
+    ('Speedy Fix', 1800, 2500),
+    ('Car Doctor', 1200, 1900),
+    ('ProMechanic', 1600, 2300),
+    ('DriveCare Garage', 1400, 2100),
+    ('GearUp Repairs', 1900, 2600),
+    ('AutoPro Solutions', 1700, 2400),
+    ('WheelWizard', 1300, 2000),
+    ('FixIt Right', 2000, 2700),
+    ('AutoTech Masters', 1450, 2150),
+    ('RoadRunner Repairs', 1550, 2250),
+    ('MotorWorks Center', 1250, 1950),
+    ('Garage Guru', 1650, 2350),
+    ('CarCraftsmen', 1350, 2050),
+    ('PitStop Pros', 1750, 2450),
+    ('Engine Envy', 2100, 2800),
+    ('WheelWhisper', 1850, 2550),
+    ('FastLane Auto', 1700, 2400),
+    ('AutoFixXpress', 1950, 2650),
+    ('CarSculptors', 1600, 2300);
+
+
